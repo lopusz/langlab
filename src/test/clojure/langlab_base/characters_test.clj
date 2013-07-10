34fa30e9e958bb3057675b2db2c88072c3ffe456
Michal Lopuszynski <lopusz@server.fake>
2013-07-10 22:45:59 +0200

    Adding tests for characters module.

---
+++ src/test/clojure/langlab_base/characters_test.clj
@@ -0,0 +1,27 @@
+(ns langlab-base.characters-test
+  (:require [clojure.test :refer :all]
+            [langlab-base.characters :refer :all]))
+
+(deftest remove-diacritics-test
+  (is (= (remove-diacritics "") ""))
+  (is (= (remove-diacritics "żółw") "zolw"))
+  (is (= (remove-diacritics "ąćęłńóżźĄĆĘŁŃÓŻŹ") "acelnozzACELNOZZ"))
+  (is (= (remove-diacritics "äöüßÄÖÜ") "aoußAOU"))
+  (is (= (remove-diacritics "àâçéèêëïîôùûüÿÀÂÇÉÈÊËÏÎÔÙÛÜŸ") 
+         "aaceeeeiiouuuyAACEEEEIIOUUUY")))
+
+(deftest count-latin-vowel-groups-test
+  (is (= (count-latin-vowel-groups "") 0))
+  (is (= (count-latin-vowel-groups "a") 1))
+  (is (= (count-latin-vowel-groups "believe") 3))
+  (is (= (count-latin-vowel-groups "größer") 2))
+  (is (= (count-latin-vowel-groups "personae") 3))
+  (is (= (count-latin-vowel-groups "algorithm") 3)))
+
+(deftest count-latin-vowel-groups-without-final-test
+  (is (= (count-latin-vowel-groups-without-final "") 0))
+  (is (= (count-latin-vowel-groups-without-final "a") 0))
+  (is (= (count-latin-vowel-groups-without-final "believe") 2))
+  (is (= (count-latin-vowel-groups-without-final "größer") 2))
+  (is (= (count-latin-vowel-groups-without-final "personae") 2))
+  (is (= (count-latin-vowel-groups-without-final "algorithm") 3)))
