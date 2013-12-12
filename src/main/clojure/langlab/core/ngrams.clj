c31016f316db6b57a2abbc5f1c8f13777ef12473
Michal Lopuszynski <lopusz@server.fake>
2013-12-12 17:39:24 +0100

    Refactoring the ngrams module and its tests.

--- src/main/clojure/langlab/core/ngrams.clj
+++ src/main/clojure/langlab/core/ngrams.clj
@@ -1,20 +1,26 @@
 (ns langlab.core.ngrams
-  "Module contains n-gram generation functions.")
+  "Module contains n-gram generation function.")
 
-(defn gen-ngrams [ tokens n ]
-  (let [
-        tokens-len (count tokens)
-        tokens-rests (take (+ (- tokens-len n) 1)
+(defn gen-ngrams
+  "Function generates n-grams from a given 'tokens' sequence.
+
+   Two invocations are possible:
+   (gen-n-grams n tokens) <- generates all n-grams
+   (gen-n-grams n m tokens) <- generates all n-grams,(n+1)-grams,...,(m)-grams
+  "
+  ([ n tokens ]
+    (let [
+          tokens-len (count tokens)
+          tokens-rests (take (+ (- tokens-len n) 1)
                          (iterate rest tokens))
-        ngrams-seq (for [ c tokens-rests ] (take n c))
-        ]
-    ngrams-seq))
-
-(defn gen-ngrams-range [ tokens nmin nmax ]
-  (let [
-        tokens-len (count tokens)
-        nmax* (min tokens-len nmax)
-        n-range (range nmin (+ nmax* 1))
-        gen-coll-ngrams (fn [n] (gen-ngrams tokens n))
-        ]
-  (mapcat gen-coll-ngrams n-range)))
+          ngrams-seq (for [ c tokens-rests ] (take n c))
+         ]
+      ngrams-seq))
+  ([ n m tokens ]
+    (let [
+          tokens-len (count tokens)
+          m* (min tokens-len m)
+          n-range (range n (+ m* 1))
+          gen-coll-ngrams #(gen-ngrams % tokens)
+         ]
+      (mapcat gen-coll-ngrams n-range))))
--- src/test/clojure/langlab/core/ngrams_test.clj
+++ src/test/clojure/langlab/core/ngrams_test.clj
@@ -1,42 +1,41 @@
 (ns langlab.core.ngrams-test
   (:require
     [ clojure.test :refer :all ]
-    [ langlab.core.ngrams :refer (gen-ngrams gen-ngrams-range)]))
+    [ langlab.core.ngrams :refer (gen-ngrams)]))
 
 (deftest test-gen-ngrams
   (is (=
-       (gen-ngrams [ ] 5)
+       (gen-ngrams 5 [ ])
        []))
   (is (=
-       (gen-ngrams [ "Alice" "Bob" "Jane" "John" ] 2)
+       (gen-ngrams 1 [ "Alice" "Bob" "Jane" "John" ])
+       [ ["Alice"] ["Bob"] ["Jane"] ["John"] ] ))
+  (is (=
+       (gen-ngrams 2 [ "Alice" "Bob" "Jane" "John" ])
        [  [ "Alice" "Bob" ]  [ "Bob" "Jane" ] [ "Jane" "John" ] ]))
-
   (is (=
-       (gen-ngrams [ "Alice" "Bob" "Jane" "John" ] 3)
+       (gen-ngrams 3 [ "Alice" "Bob" "Jane" "John" ])
        [  [ "Alice" "Bob" "Jane" ] [ "Bob" "Jane" "John" ] ]))
-
   (is (=
-       (gen-ngrams [ "Alice" "Bob" ] 3)
-       [ ])))
-
-(deftest test-gen-ngrams-range
+       (gen-ngrams 3 [ "Alice" "Bob" ])
+       [ ]))
   (is (=
-       (gen-ngrams-range [ ] 5 7)
+       (gen-ngrams 5 7 [ ])
        []))
   (is (=
-       (gen-ngrams-range [ "Alice" "Bob" "Jane" "John" ] 2 4)
+       (gen-ngrams 2 4 [ "Alice" "Bob" "Jane" "John" ])
        [ [ "Alice" "Bob" ]  [ "Bob" "Jane"]  [ "Jane" "John" ]
          [ "Alice" "Bob" "Jane" ] [ "Bob" "Jane" "John" ]
          [ "Alice" "Bob" "Jane" "John" ] ]))
   (is (=
-       (gen-ngrams-range [ "Alice" "Bob" "Jane" "John" ] 2 10)
+       (gen-ngrams 2 10 [ "Alice" "Bob" "Jane" "John" ])
        [ [ "Alice" "Bob" ]  [ "Bob" "Jane"]  [ "Jane" "John" ]
          [ "Alice" "Bob" "Jane" ] [ "Bob" "Jane" "John" ]
          [ "Alice" "Bob" "Jane" "John" ] ] ))
   (is (=
-       (gen-ngrams-range [ "Alice" "Bob" "Jane" "John" ] 2 2)
+       (gen-ngrams 2 2 [ "Alice" "Bob" "Jane" "John" ])
        [ [ "Alice" "Bob" ]  [ "Bob" "Jane" ] [ "Jane" "John" ]  ]))
 
   (is (=
-       (gen-ngrams-range [ "Alice" "Bob" "Jane" "John" ] 3 1)
+       (gen-ngrams 3 1 [ "Alice" "Bob" "Jane" "John" ])
        [ ] )))
