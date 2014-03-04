7daffaa8f40f70ead2e0da2bb7d0eeb6cf7ab27c
Michal Lopuszynski <lopusz@server.fake>
2014-03-04 10:45:37 +0100

    Reverting to slightly faster ngrams generation (not using partition).

--- src/main/clojure/langlab/core/ngrams.clj
+++ src/main/clojure/langlab/core/ngrams.clj
@@ -4,6 +4,34 @@
 (defn gen-ngrams
   "Function generates n-grams from a given 'tokens' sequence.
 
+   Two invocations are possible:
+   (gen-n-grams n tokens) <- generates all n-grams
+   (gen-n-grams n m tokens) <- generates all n-grams,(n+1)-grams,...,(m)-grams
+   (gen-ngrams tokens) <- generates 1 .. (count tokens) n-grams
+  "
+  ([ tokens ] (gen-ngrams 1 (count tokens) tokens))
+  ([ n tokens ]
+    (let [
+          tokens-len (count tokens)
+          tokens-rests (take (+ (- tokens-len n) 1)
+                         (iterate rest tokens))
+          ngrams-seq (for [ c tokens-rests ] (take n c))
+         ]
+      ngrams-seq))
+  ([ n m tokens ]
+    (let [
+          tokens-len (count tokens)
+          m* (min tokens-len m)
+          n-range (range n (+ m* 1))
+          gen-coll-ngrams #(gen-ngrams % tokens)
+         ]
+      (mapcat gen-coll-ngrams n-range))))
+
+(comment
+
+(defn gen-ngrams*
+  "Function generates n-grams from a given 'tokens' sequence.
+
    Following invocations are possible:
    (gen-ngrams n tokens) <- generates all n-grams
    (gen-ngrams n m tokens) <- generates all n-grams,(n+1)-grams,...,(m)-grams
@@ -18,4 +46,4 @@
   ([ n m tokens ]
     (mapcat
       #(gen-ngrams % tokens)
-      (range n (inc m)))))
+      (range n (inc m))))))
